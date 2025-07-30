6b7e61f3ea1a693562e5f196d8e9aacd5b39b6e4
Antonio <toony1908@gmail.com>
2025-07-30 12:45:10 -0600

    aDDD

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest {
     public void testHelloWorldEndpoint() {
         Javalin app = App.createApp();
         JavalinTest.test(app, (server, client) -> {
-            assertThat(client.get("/hellox").body().string()).isEqualTo("Hello World");
+            assertThat(client.get("/helloxs").body().string()).isEqualTo("Hello World");
         });
     }
 }
