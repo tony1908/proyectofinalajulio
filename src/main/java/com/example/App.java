10cdd5f9c55cd88395322fce3e9ef1d98b4a6a9c
Antonio <toony1908@gmail.com>
2025-07-30 10:37:18 -0600

    Add javalin basic server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,18 @@
 package com.example;
 
+import io.javalin.Javalin;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static Javalin createApp() {
+        return Javalin.create().get("/hello", ctx -> ctx.result("Hello World"));
+    }
+
+    public static void main(String[] args) {
+        createApp().start(7000);
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,19 @@
 package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+    @Test
+    public void testHelloWorldEndpoint() {
+        Javalin app = App.createApp();
+        JavalinTest.test(app, (server, client) -> {
+            assertThat(client.get("/hello").body().string()).isEqualTo("Hello World");
+        });
     }
 }
